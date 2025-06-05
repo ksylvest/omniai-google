8ec833e2761c4e4f90c0ae505b414cfa8020f2f1
Kevin Sylvestre <kevin@ksylvest.com>
2025-06-05 10:09:22 -0700

    Bump Gemini 2.5 pro model ID

--- lib/omniai/google/chat.rb
+++ lib/omniai/google/chat.rb
@@ -15,7 +15,7 @@ module OmniAI
       module Model
         GEMINI_1_0_PRO = "gemini-1.0-pro"
         GEMINI_1_5_PRO = "gemini-1.5-pro"
-        GEMINI_2_5_PRO = "gemini-2.5-pro-preview-05-06"
+        GEMINI_2_5_PRO = "gemini-2.5-pro-preview-06-05"
         GEMINI_1_5_FLASH = "gemini-1.5-flash"
         GEMINI_2_0_FLASH = "gemini-2.0-flash"
         GEMINI_2_5_FLASH = "gemini-2.5-flash-preview-04-17"
